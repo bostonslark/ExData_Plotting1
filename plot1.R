0fb8aa4fe4eae9d98ac8d4e06959cce2b5fb5aa3
Xinchen Chen <42183839+bostonslark@users.noreply.github.com>
2018-08-11 17:02:39 -0400

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,5 @@
+header <- read.table('household_power_consumption.txt', sep = ';', header = FALSE, nrows = 1, stringsAsFactors = FALSE)
+hpc <- read.table('household_power_consumption.txt', sep = ';', header = FALSE, skip = 66637, nrows = 2880, stringsAsFactors = FALSE, col.names = header)
+png('plot1.png', width = 480, height = 480)
+hist(hpc$Global_active_power, main = 'Global Active Power', xlab = 'Global Active Power (kilowatts)', col = 'red')
+dev.off()
